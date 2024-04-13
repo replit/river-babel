29decb7ae510edd33d40db30cf4a94e958b74faa
zhenthebuilder <zhen@repl.it>
2024-04-12 17:11:12 -0700

    Add tests for network disconnect situation

---
+++ tests/test_network.ts
@@ -0,0 +1,59 @@
+
+
+import type { Test } from "../src/actions";
+
+export const NetworkDisconnectTest: Test = {
+  client: {
+    actions: [
+      { type: "invoke", id: "1", proc: "kv.set", payload: { k: "foo", v: 42 } },
+      { type: "invoke", id: "2", proc: "kv.set", payload: { k: "foo", v: 43 } },
+      { type: "disconnect_network" },
+      { type: "connect_network" },
+      { type: "invoke", id: "3", proc: "kv.set", payload: { k: "abc", v: 1 } },
+    ],
+    expectedOutput: [
+      { id: "1", status: "ok", payload: 42 },
+      { id: "2", status: "ok", payload: 43 },
+      { id: "3", status: "ok", payload: 1 },
+    ],
+  }
+}
+
+
+export const BufferRequestTest: Test = {
+  client: {
+    actions: [
+      { type: "invoke", id: "1", proc: "kv.set", payload: { k: "foo", v: 42 } },
+      { type: "disconnect_network" },
+      { type: "invoke", id: "2", proc: "kv.set", payload: { k: "foo", v: 43 } },
+      { type: "invoke", id: "3", proc: "kv.set", payload: { k: "abc", v: 1 } },
+      { type: "connect_network" },
+    ],
+    expectedOutput: [
+      { id: "1", status: "ok", payload: 42 },
+      { id: "2", status: "ok", payload: 43 },
+      { id: "3", status: "ok", payload: 1 },
+    ],
+  }
+}
+
+
+export const SubscriptionDisconnectTest: Test = {
+  client: {
+    actions: [
+      { type: "invoke", id: "1", proc: "kv.set", payload: { k: "foo", v: 42 } },
+      { type: "invoke", id: "2", proc: "kv.watch", payload: { k: "foo" } },
+      { type: "disconnect_network" },
+      { type: "invoke", id: "3", proc: "kv.set", payload: { k: "abc", v: 1 } },
+      { type: "invoke", id: "4", proc: "kv.set", payload: { k: "foo", v: 43 } },
+      { type: "connect_network" },
+    ],
+    expectedOutput: [
+      { id: "1", status: "ok", payload: 42 },
+      { id: "2", status: "ok", payload: 42 }, // watch should return the current value
+      { id: "3", status: "ok", payload: 1 },
+      { id: "2", status: "ok", payload: 43 },
+      { id: "4", status: "ok", payload: 43 },
+    ],
+  },
+};
