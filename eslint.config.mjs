251a6429ed2d26879cc17d94fb0820d42c938255
Devon Stewart <devon.stewart@repl.it>
2024-11-13 14:19:46 -0800

    Enable eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ["impls/python/.venv/**"]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    languageOptions: {
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-floating-promises": ["error"],
+        "array-callback-return": ["error"],
+    },
+  },
+);
+
