47e85e341f05000ad742c91ef7c959350cd9403d
Devon Stewart <devon.stewart@repl.it>
2025-03-20 19:34:39 +0000

    Adding eslint to node impls

--- impls/node/eslint.config.mjs
+++ impls/node/eslint.config.mjs
@@ -2,9 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['impls/**'],
-  },
+  {},
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
